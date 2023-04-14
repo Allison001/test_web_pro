9cda56bcf66329353e40b45524aafa4c5a63dbb1
Allison001 <zhangyingxbba@gmail.com>
2023-04-14 18:50:16 +0800

    测试

--- test_web/test_web.py
+++ test_web/test_web.py
@@ -8,5 +8,5 @@ class TestWeb():
         self.driver = webdriver.Chrome()
         self.driver.get("http://www.baidu.com")
         time.sleep(5)
-        self.driver.implicitly_wait(3)
+        self.driver.implicitly_wait(5)
         self.driver.quit()
